015c3fcf70cd7ef35a022f274721db69580884fd
Popoola Seyitan <60761852+Watermelon-Sugar@users.noreply.github.com>
2022-02-10 12:03:10 +0100

    reaction timer

---
+++ src/components/Block.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+      Click Me
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        }, this.delay)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            this.$emit('ended', this.reactionTime)
+        }
+    }
+    // updated() {
+    //     console.log('updated')
+    // },
+    // unmounted() {
+    //     console.log('unmounted')
+    // },
+}
+</script>
+
+<style>
+    .block {
+        width: 300px;
+        border-radius: 20px;
+        background:  rgb(211, 152, 41);
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+        color: aliceblue;
+    }
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <p >Reaction Time: {{score}}ms</p>
+  <p class="rank">{{rank}} </p>
+</template>
+
+<script>
+export default {
+    props: ['score'], 
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Sharp!'
+        } else if (this.score < 1000 ) {
+            this.rank = "Fast!"
+        } else if (this.score < 3000 ) {
+            this.rank = "Good Going!"
+        } else {
+            this.rank = "Slug!"
+        }
+    },
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: chocolate;
+        font-weight: bold;
+    }
+</style>
